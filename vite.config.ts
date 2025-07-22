55243ab3d7db93284c7d691a8f93dca6fd3ea59e
nurakash16 <nurakash16@gmail.com>
2025-07-22 11:35:22 +0600

    Fix client build and vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from "vite";
+const { defineConfig } = require('vite');
+
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
